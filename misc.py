9d1fc92d9f954bf7d1547808068d71edaabcb11f
sa6ok2003 <73820004+sa6ok2003@users.noreply.github.com>
2021-05-29 11:06:43 +0300

    Add files via upload

---
+++ misc.py
@@ -0,0 +1,13 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+
+TOKEN = '1856979718:AAHiqOY5W6mpfOBnEkaYggdHfyiBwEj5vos'
+memory_storage = MemoryStorage()
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot,storage=memory_storage)
+logging.basicConfig(level=logging.INFO)
